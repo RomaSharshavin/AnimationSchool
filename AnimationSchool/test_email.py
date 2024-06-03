6aad5bbe9bce67f643ad6a72f90e20daf0050684
Margo <Margo@LAPTOP-MARGO>
2024-06-03 19:00:47 +0300

    Готовый код тестов для почты

--- AnimationSchool/test_email.py
+++ AnimationSchool/test_email.py
@@ -1,16 +1,36 @@
 import unittest
-import myform
+from myform import is_valid_email
 
-class Test_test_email(unittest.TestCase):
-    def test_A(self):
-        list_mail_uncor = ["", "1", "@", "@mail", "example@mail", "@@", "Nikita//@gmail", "\$jdewnce@gmail.com", "!defxyz%%%%%%%%%@example.com"]
-        for x in list_mail_uncor:
-            self.assertFalse(myform.mail(x))
-            
-    def test_A(self):
-        list_mail_uncor = ["m.m@mail.ru", "Exampl@gmail.com", "11nikita12@gmail.com", "E23xdfghbvf@mail.ru", "example@mail.bk.com"]
-        for x in list_mail_uncor:
-            self.assertTrue(myform.mail(x))
+class TestEmailValidation(unittest.TestCase):
+    def test_valid_emails(self):
+        valid_emails = ["m.m@mail.ru", 
+                        "m1@gmail.com", 
+                        "lil%11@gmail.com", 
+                        "lil-uchi18%n@mail.ru", 
+                        "john.doe@example.com", 
+                        "bob123@yahoo.co.jp", 
+                        "nathan.miller@mydomain.org", 
+                        "peter_parkinson123@gmail.com",
+                        "emma-jones@example.net", 
+                        "maria_garcia123@example.com"]
+        for email in valid_emails:
+            with self.subTest(email=email):
+                self.assertTrue(is_valid_email(email))
+
+    def test_invalid_emails(self):
+        invalid_emails = ["", 
+                          "1", 
+                          "m1@", 
+                          "m1@@mail", 
+                          "mail.com", 
+                          "m@mail.", 
+                          "m@.com", 
+                          "m@com", 
+                          "m1@.mail.com", 
+                          "m@mail..com"]
+        for email in invalid_emails:
+            with self.subTest(email=email):
+                self.assertFalse(is_valid_email(email))
 
 if __name__ == '__main__':
     unittest.main()
